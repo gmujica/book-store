ae0064492fda608f137f2935ac5fe36c765239b2
gmujica <gregorymujica1993@gmail.com>
2022-05-07 20:17:53 -0500

    user module created

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,28 @@
+import { timestamp } from "rxjs";
+import { BaseEntity, Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User extends BaseEntity {
+    
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({ type: 'varchar', unique: true, length:, nullable: false })
+    username: string;
+
+    @Column({ type: 'varchar', nullable: false })
+    email: string;
+
+    @Column({ type: 'varchar', nullable: false })
+    password: string;
+
+    @Column({ type: 'varchar', default: 'ACTIVE', length: 8 })
+    status: string;
+
+    @Column({ type: 'timestamp', name: 'create_at' })
+    createAt: Date;
+
+    @Column({ type: 'timestamp', name: 'update_at' })
+    updateAt: Date;
+
+}
